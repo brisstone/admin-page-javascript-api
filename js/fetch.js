1f8048f36ae69f3830d9b61b131b6dad2ec4d575
brisstone <okolijohnson69@gmail.com>
2022-01-20 13:40:10 +0100

    updt

--- js/fetch.js
+++ js/fetch.js
@@ -29,122 +29,59 @@ fetch(url)
     console.log(allUsers, 'dskssjks')
     const tableBody = table.querySelector('tbody')
 
+    // const rowElement =document.createElement('tr');
+    //   rowElement.classList.add("active-row");
 
 
-  
-  
+    for(var i = 0; i < allUsers.length; i++){
 
-    //clear the table first, in case of page refresh on data
-    // tableBody.innerHTML = '<tr><tr>'
-
-    
-
-
-    // allUsers.map(item => `<tr><td>${item.email}</td><td>${item.company}</td></tr>`)
-
-    // allUsers.map(function(user) {
-    //     // let tdName = document.createElement('td');
-    //     // let tdCompany = document.createElement('td');
-    //     // let name = document.createElement('h2');
-    //     // let email = document.createElement('span');
-
-    //     // const tableBody = table.querySelector('tbody')
-    //     // //clear the table first, in case of page refresh on data
-    //     // tableBody.innerHTML = '<tr><tr>'
-
-    //     // name.innerHTML = `${user.email}`;
-    //     // email.innerHTML = `${user.company}`;
-
-    //     console.log(user, 'kdkdkkd')
-    //     const bodyElement = document.createElement('td')
-    //     bodyElement.textContent = user
-    //     rowElement.appendChild(bodyElement)
+      
+      var row = `<tr class='active-row' >
+                <td>${allUsers[i].email}</td>
+                <td>${allUsers[i].first_name}</td>
+                <td>${allUsers[i].last_name}</td>
+                <td>${allUsers[i].company}</td>
+               
+      
+      
+                </tr>`
+      table.innerHTML += row
 
 
-    //     // td.appendChild(tdName);
-    //     // td.appendChild(tdCompany);
-    //     // list.appendChild(td);
-    //   });
-
-      // tr.appendChild(list);
-
-
-      // const rowElement =document.createElement('tr');
-
+    }
 
       
 
-      for (const user of allUsers){
+      // for (const user of allUsers){
         // console.log(company)
-        const rowElement =document.createElement('tr');
-        rowElement.classList.add("active-row");
-
-        // const first_name =document.createElement('tr');
-        // const last_name =document.createElement('tr');
-        // const company =document.createElement('tr');
 
 
-
-
-        const bodyElement = document.createElement('td')
-        const Elementfirst_name = document.createElement('td')
-        const Elementlast_name = document.createElement('td')
-        const Elementcompany = document.createElement('td')
-
-        bodyElement.textContent = user.email
-        Elementfirst_name.textContent = user.first_name
-        Elementlast_name.textContent = user.last_name
-        Elementcompany.textContent = user.company
-
-        rowElement.appendChild(bodyElement)
-        rowElement.appendChild(Elementfirst_name)
-        rowElement.appendChild(Elementlast_name)
-        rowElement.appendChild(Elementcompany)
-
-        tableBody.appendChild(rowElement)
-        // tableBody.appendChild(first_name)
-        // tableBody.appendChild(last_name)
-        // tableBody.appendChild(company)
-
-
-////////////////////////////////////////////////////////////////
+    
         // const rowElement =document.createElement('tr');
-        // for (var colIndex = 0; colIndex < user.length; colIndex++)
-        // {
+        // rowElement.classList.add("active-row");
 
-          
-        //     var val = user[i][cols[colIndex]];
-        //     console.log(user[i], 'kskkss')
-        //     // If there is any key, which is matching
-        //     // with the column name
-        //     if (val == null) val = ""; 
-        //     rowElement.append($('<td/>').html(val));
-        //     tableBody.appendChild(rowElement)
+      
 
-        // }
-////////////////////////////////////////////////////////////
-        // const rowElementData = document.createElement('td')
-        // rowElementData.innerText = user.email
-        // rowElement.appendChild(rowElementData)
+
+
+
+        // const bodyElement = document.createElement('td')
+        // const Elementfirst_name = document.createElement('td')
+        // const Elementlast_name = document.createElement('td')
+        // const Elementcompany = document.createElement('td')
+
+        // bodyElement.textContent = user.email
+        // Elementfirst_name.textContent = user.first_name
+        // Elementlast_name.textContent = user.last_name
+        // Elementcompany.textContent = user.company
+
+        // rowElement.appendChild(bodyElement)
+        // rowElement.appendChild(Elementfirst_name)
+        // rowElement.appendChild(Elementlast_name)
+        // rowElement.appendChild(Elementcompany)
+
         // tableBody.appendChild(rowElement)
-
-
-
-        // const bodyElementCompany = document.createElement('td')
-        // bodyElementCompany.textContent = user.company
-        // rowElement.appendChild(bodyElementCompany)
-        
-
-      //   for (const cellText of company){
-
-          
-      //   const bodyElement = document.createElement('td')
-      //   bodyElement.textContent = cellText
-      //   rowElement.appendChild(bodyElement)
-
-      //   }
-
-      // }
+   
   
-      }
+      // }
   }   
